5c0e6aa465413263d53ef728e2619ca4e72c38bc
Alvaro Bautista <67112345+alvrba@users.noreply.github.com>
2022-01-11 10:36:56 +0100

    Delete schemas.ts

--- schemas.ts
+++
@@ -1,8 +0,0 @@
-/*import S from 'fluent-json-schema';
-
-const idParam = S.object().prop('id', uuid).required(['id']);
-
-const getOne = {
-  params: idParam,
-  response: { 200: item, '4xx': error },
-};*/
