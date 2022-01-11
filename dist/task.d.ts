de266c6358f60b9a38617d7c325e9e66c58ce43f
Alvaro Bautista <67112345+alvrba@users.noreply.github.com>
2022-01-11 10:35:49 +0100

    Delete dist directory

--- dist/task.d.ts
+++
@@ -1,8 +0,0 @@
-import { DatabaseTransactionHandler, UnknownExtra } from 'graasp';
-import { ActionService } from './db-service';
-import { BaseActionTask } from './base-task';
-import { Action } from './interfaces/actions';
-export declare class GetActionTask<E extends UnknownExtra> extends BaseActionTask<Action> {
-    constructor(actionService: ActionService);
-    run(handler: DatabaseTransactionHandler): Promise<void>;
-}
